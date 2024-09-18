0f960e8ab367ba2f7d95d96231771e25d80184f2
Mikhail Kultyiasau <mikhail.kultiyasov@tispr.com>
2024-09-18 15:08:10 +0200

    Implement fetch trades from binance

--- src/binance/binance.service.ts
+++ src/binance/binance.service.ts
@@ -1,7 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { TradesResponse } from './models/trades-response';
+import { ConfigService } from '@nestjs/config';
+import { SymbolEnum } from '../types/symbol-enum';
+import axios, { AxiosResponse } from 'axios';
 
 @Injectable()
 export class BinanceService {
-	async fetchAndStoreTrades(): Promise<TradesResponse> {}
+	private binanceApiUrl: string;
+	private logger: Logger;
+
+	constructor(private readonly configService: ConfigService) {
+		this.binanceApiUrl = configService.get('BINANCE_API_URL');
+		this.logger = new Logger(BinanceService.name);
+	}
+	async fetchAndStoreTrades(symbol: SymbolEnum): Promise<TradesResponse> {
+		try {
+			const response: AxiosResponse<TradesResponse> = await axios.get<TradesResponse>(
+				`${this.binanceApiUrl}/api/v3/trades`,
+				{
+					params: {
+						symbol,
+					},
+				},
+			);
+
+			return response;
+		} catch (error) {
+			this.logger.error('Failed to fetching trades', error);
+			throw new InternalServerErrorException('Failed to fetching trades');
+		}
+	}
 }
